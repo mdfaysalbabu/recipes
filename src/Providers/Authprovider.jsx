bc7c4171bf6a8da4102db174a0a35bfbc50448da
mdfaysalbabu <foysalfoysal313@gmail.com>
2023-05-02 16:39:04 +0530

    add firebase

---
+++ src/Providers/Authprovider.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../../firebase.config';
+
+export const authContext=createContext(null)
+const auth=getAuth(app)
+const Authprovider = ({children}) => {
+    const [user,setuser]=useState(null)
+    const [loading,setloading]=useState(true)
+    const creatUser=(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const login=(email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const logout=()=>{
+        return signOut(auth)
+    }
+    const authInfo={
+        user,
+        creatUser,
+        login,
+        logout,
+        loading
+    }
+    
+    return (
+        <authContext.Provider value={authInfo}>
+           {children} 
+        </authContext.Provider>
+    );
+};
+
+export default Authprovider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './Routes/Routes.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import Authprovider from "./Providers/Authprovider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <Authprovider>
+      <RouterProvider router={router}></RouterProvider>
+    </Authprovider>
+  </React.StrictMode>
+);
